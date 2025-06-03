02709d2d57785cecf76c44027e41efdac82b5700
Markus <mhein@multamedio.de>
2025-06-03 12:05:53 +0200

    removed rewrites

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 // This file contains JavaScript code for the Hello World website.
 
-// Rewrite rule: Remove /applink prefix from the path if present
-function handleApplinkRewrite() {
-    const path = window.location.pathname;
-    if (path.startsWith('/applink')) {
-        const newPath = path.replace(/^\/applink/, '') || '/';
-        const newUrl = newPath + window.location.search + window.location.hash;
-        window.location.replace(newUrl);
-    }
-}
+// // Rewrite rule: Remove /applink prefix from the path if present
+// function handleApplinkRewrite() {
+//     const path = window.location.pathname;
+//     if (path.startsWith('/applink')) {
+//         const newPath = path.replace(/^\/applink/, '') || '/';
+//         const newUrl = newPath + window.location.search + window.location.hash;
+//         window.location.replace(newUrl);
+//     }
+// }
 
 handleApplinkRewrite();
 
